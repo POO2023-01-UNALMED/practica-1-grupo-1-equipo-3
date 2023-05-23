a6858952e5ce1abe12d0b42bfacaa83996176430
Cartosn <91155360+Cartosn@users.noreply.github.com>
2023-05-23 18:42:29 -0500

    infraestructura comentada

--- Banco/src/gestorAplicacion/infraestructura/Banco.java
+++ Banco/src/gestorAplicacion/infraestructura/Banco.java
@@ -21,68 +21,111 @@ import java.util.List;
 import java.util.Locale;
 
 
-public class Banco implements Serializable{
+public class Banco implements Serializable {
 	@Serial
 	private static final long serialVersionUID = 1L;
-	
+
 	private static List<Cliente> clientes = new ArrayList<>();
 	private static List<Canal> canales = new ArrayList<>();
-	
+
 	public Banco() {
 		Deserializador.deserializar(this);
 	}
-	
-	//Metodos de la clase
-	
-	public static ArrayList<Canal> ordenarCanalesPorImpuestos(List<Canal> canales) {//De menor a mayor
-	    canales.sort(Comparator.comparingDouble(Canal::getImpuesto));
-	    return new ArrayList<>(canales);
-	}
-	
-	//Getters & Setters
-	/*Los getters y los setters contienen metodos que permiten, por ejemplo
-	 * Eliminar un solo usuario del arrayList o eliminar varios (metodos separados)
+
+	// Métodos de la clase
+
+	/**
+	 * Ordena una lista de canales por impuestos de menor a mayor.
+	 *
+	 * @param canales Lista de canales a ordenar.
+	 * @return ArrayList ordenado de canales por impuestos.
 	 */
-	
+	public static ArrayList<Canal> ordenarCanalesPorImpuestos(List<Canal> canales) {
+		canales.sort(Comparator.comparingDouble(Canal::getImpuesto));
+		return new ArrayList<>(canales);
+	}
+
+	// Getters & Setters
+
+	/**
+	 * Obtiene la lista de clientes del banco.
+	 *
+	 * @return ArrayList de clientes.
+	 */
 	public static ArrayList<Cliente> getClientes() {
 		return (ArrayList<Cliente>) clientes;
 	}
-	
+
+	/**
+	 * Establece la lista de clientes del banco.
+	 *
+	 * @param clientes ArrayList de clientes a establecer.
+	 */
 	public static void setClientes(ArrayList<Cliente> clientes) {
 		Banco.clientes = clientes;
 	}
-	
-	public static void agregarCliente(Cliente cliente) {//Agregar un solo cliente
+
+	/**
+	 * Agrega un cliente a la lista de clientes del banco.
+	 *
+	 * @param cliente Cliente a agregar.
+	 */
+	public static void agregarCliente(Cliente cliente) {
 		Banco.clientes.add(cliente);
 	}
 
-
+	/**
+	 * Obtiene la lista de canales del banco.
+	 *
+	 * @return ArrayList de canales.
+	 */
 	public static ArrayList<Canal> getCanales() {
 		return (ArrayList<Canal>) canales;
 	}
-	
+
+	/**
+	 * Establece la lista de canales del banco.
+	 *
+	 * @param canales ArrayList de canales a establecer.
+	 */
 	public static void setCanales(ArrayList<Canal> canales) {
 		Banco.canales = canales;
 	}
-	
-	public static void agregarCanal(Canal canal) {//Agregar un solo canal
+
+	/**
+	 * Agrega un canal a la lista de canales del banco.
+	 *
+	 * @param canal Canal a agregar.
+	 */
+	public static void agregarCanal(Canal canal) {
 		Banco.canales.add(canal);
 	}
 
-
-	public static int calcularPuntaje(ArrayList<Transaccion> trans){
+	/**
+	 * Calcula el puntaje de un cliente basado en las transacciones realizadas.
+	 *
+	 * @param trans ArrayList de transacciones del cliente.
+	 * @return Puntaje calculado.
+	 */
+	public static int calcularPuntaje(ArrayList<Transaccion> trans) {
 		int puntaje = 0;
-		for(Transaccion t : trans){
-			puntaje += 2 *t.getCantidad()*t.getDivisa().getValor();
+		for (Transaccion t : trans) {
+			puntaje += 2 * t.getCantidad() * t.getDivisa().getValor();
 		}
 		return puntaje;
 	}
 
-	public static ArrayList<Divisa> seleccionarDivisa(Cliente cliente){
+	/**
+	 * Selecciona las divisas utilizadas por un cliente.
+	 *
+	 * @param cliente Cliente del cual seleccionar las divisas.
+	 * @return ArrayList de divisas seleccionadas.
+	 */
+	public static ArrayList<Divisa> seleccionarDivisa(Cliente cliente) {
 		ArrayList<Divisa> retorno = new ArrayList<>();
-		for(Divisa d : Divisa.values()){
-			for(Tarjeta t : cliente.getTarjetas()){
-				if(t.getDivisa().equals(d)){
+		for (Divisa d : Divisa.values()) {
+			for (Tarjeta t : cliente.getTarjetas()) {
+				if (t.getDivisa().equals(d)) {
 					retorno.add(d);
 					break;
 				}
@@ -91,9 +134,15 @@ public class Banco implements Serializable{
 		return retorno;
 	}
 
-	public static boolean numeroExistente(int num) { // Evalua si un número de tarjeta corresponde a algúna tarjeta de algún cliente
+	/**
+	 * Verifica si un número de tarjeta existe en alguna tarjeta de algún cliente.
+	 *
+	 * @param num Número de tarjeta a verificar.
+	 * @return true si el número de tarjeta existe, false en caso contrario.
+	 */
+	public static boolean numeroExistente(int num) {
 		boolean valor = false;
-		for(Tarjeta t : Tarjeta.getTarjetas()) {
+		for (Tarjeta t : Tarjeta.getTarjetas()) {
 			if (num == t.getNoTarjeta()) {
 				valor = true;
 				break;
@@ -102,21 +151,28 @@ public class Banco implements Serializable{
 		return valor;
 	}
 
-	public static void generarPeticion(Transaccion transaccion, String mensaje){
+	/**
+	 * Genera una petición de transacción y la guarda en una instancia de Transaccion.
+	 *
+	 * @param transaccion Transacción a generar la petición.
+	 * @param mensaje     Mensaje asociado a la petición.
+	 */
+	public static void generarPeticion(Transaccion transaccion, String mensaje) {
 		transaccion.setRetornable(false);
 		new Transaccion(transaccion, mensaje);
 	}
-	
+
 	/**
-	 * Esta funcion es con el fin de usarse en los toString y las impresiones en consola
-	 * @param numero a formatear
-	 * @return el numero formateado de acuerdo a las reglas de formato de Colombia. 
-	 * Ejm: 1000000.25 = 1.000.000,25
-	 * */
+	 * Formatea un número de acuerdo a las reglas de formato de Colombia.
+	 *
+	 * @param numero Número a formatear.
+	 * @return El número formateado de acuerdo a las reglas de formato de Colombia.
+	 *         Ejemplo: 1000000.25 = 1.000.000,25
+	 */
 	public static String formatearNumero(double numero) {
 		Locale esLocale = new Locale("es", "CO");
-        DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(esLocale);
-        df.applyPattern("#,##0.00");
-        return df.format(numero);
-    }
+		DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(esLocale);
+		df.applyPattern("#,##0.00");
+		return df.format(numero);
+	}
 }
--- Banco/src/gestorAplicacion/infraestructura/Canal.java
+++ Banco/src/gestorAplicacion/infraestructura/Canal.java
@@ -12,105 +12,165 @@ import java.util.EnumMap;
 import gestorAplicacion.entidades_de_negocio.*;
 import gestorAplicacion.tarjetas.*;
 
-public class Canal implements Serializable{
+public class Canal implements Serializable {
 	@Serial
 	private static final long serialVersionUID = 1L;
-	
+
 	private final String tipoCanal;
-	
-	/*	Tipos de Canales:
-	 *	- Cajero Automatico: Solo tienen una divisa disponible. Estos canales no pueden cambiar divisas. Solo se puede retirar de ellos
-	 *	- Sucursal Fisica: Tienen todas las divisas disponibles. Pueden cambiar todas las divisas.
-	 *	- Sucursal en Linea: Tienen todas las divisas disponibles. Pueden cambiar todas las divisas. 
-	 *	- Corresponsal Bancario: Solo tienen 3 divisas, dos de ellas son las monedas internacionales mas importantes (DOLAR y EURO) y una tercera que puede ser cualquier otra.
-	 *	Por lo que solo se pueden cambiar esas 3 divisas en estos canales
-	 * */
+
+	/**
+	 * Tipos de Canales:
+	 * - Cajero Automático: Solo tienen una divisa disponible. Estos canales no pueden cambiar divisas. Solo se puede retirar dinero de ellos.
+	 * - Sucursal Física: Tienen todas las divisas disponibles. Pueden cambiar todas las divisas.
+	 * - Sucursal en Línea: Tienen todas las divisas disponibles. Pueden cambiar todas las divisas.
+	 * - Corresponsal Bancario: Solo tienen 3 divisas, dos de ellas son las monedas internacionales más importantes (DÓLAR y EURO) y una tercera que puede ser cualquier otra.
+	 *   Por lo tanto, solo se pueden cambiar esas 3 divisas en estos canales.
+	 */
 	private final float impuesto;
 	private final EnumMap<Divisa, Double> fondosPorDivisa = new EnumMap<>(Divisa.class);
 
-	//Los fondos deben ser proporcionados en el orden en que estan declaradas las divisas
-    public Canal(String tipoCanal, float impuesto, double... fondos) {
-    	this(tipoCanal, impuesto);
-    	
-    	for(int i = 0; i < Divisa.values().length && i < fondos.length; i++) {
-    		Divisa divisa = Divisa.values()[i];
-    		double fondo = fondos[i];
-    		this.fondosPorDivisa.put(divisa, fondo);
-    	}
-    }
-    
-    //Constructor para configurar solo el tipo de canal y los impuestos, los fondos seran configurados uno por uno
-    //Esto con la idea de inicializar canales que solo tengan algunas divisas
-    public Canal(String tipoCanal, float impuesto) {
-    	this.tipoCanal = tipoCanal;
-    	this.impuesto = impuesto;
-    	
-    	Banco.agregarCanal(this);
-    }
-    
-    //Getters y Setters
-    public void setFondos(Divisa divisa, double monto) {
-        this.fondosPorDivisa.put(divisa, monto);
-    }
-    
-    public double getFondos(Divisa divisa) {
-        return this.fondosPorDivisa.get(divisa);
-    }
+	/**
+	 * Constructor de la clase Canal.
+	 *
+	 * @param tipoCanal El tipo de canal (Cajero Automático, Sucursal Física, Sucursal en Línea, Corresponsal Bancario).
+	 * @param impuesto  La tasa de impuestos asociada al canal.
+	 * @param fondos    Los fondos disponibles en el canal para cada divisa en el orden en que están declaradas las divisas.
+	 */
+	public Canal(String tipoCanal, float impuesto, double... fondos) {
+		this(tipoCanal, impuesto);
 
+		for (int i = 0; i < Divisa.values().length && i < fondos.length; i++) {
+			Divisa divisa = Divisa.values()[i];
+			double fondo = fondos[i];
+			this.fondosPorDivisa.put(divisa, fondo);
+		}
+	}
 
+	/**
+	 * Constructor de la clase Canal sin fondos inicializados.
+	 *
+	 * @param tipoCanal El tipo de canal (Cajero Automático, Sucursal Física, Sucursal en Línea, Corresponsal Bancario).
+	 * @param impuesto  La tasa de impuestos asociada al canal.
+	 */
+	public Canal(String tipoCanal, float impuesto) {
+		this.tipoCanal = tipoCanal;
+		this.impuesto = impuesto;
+
+		Banco.agregarCanal(this);
+	}
+
+	// Getters y Setters
+
+	/**
+	 * Establece los fondos disponibles para una divisa específica en el canal.
+	 *
+	 * @param divisa La divisa para la cual se establecerán los fondos.
+	 * @param monto  El monto de fondos disponibles para la divisa.
+	 */
+	public void setFondos(Divisa divisa, double monto) {
+		this.fondosPorDivisa.put(divisa, monto);
+	}
+
+	/**
+	 * Obtiene los fondos disponibles para una divisa específica en el canal.
+	 *
+	 * @param divisa La divisa para la cual se obtendrán los fondos.
+	 * @return El monto de fondos disponibles para la divisa.
+	 */
+	public double getFondos(Divisa divisa) {
+		return this.fondosPorDivisa.get(divisa);
+	}
+
+	/**
+	 * Obtiene la tasa de impuestos asociada al canal.
+	 *
+	 * @return La tasa de impuestos del canal.
+	 */
 	public double getImpuesto() {
 		return impuesto;
 	}
 
-	//Metodos de las instancias
-	
+	// Métodos de las instancias
+
+	/**
+	 * Devuelve una representación en forma de cadena del objeto Canal.
+	 *
+	 * @return La representación en forma de cadena del objeto Canal.
+	 */
 	@Override
 	public String toString() {
-		return "Canal: %s #%s\nTasa de impuestos: %s\n".formatted(tipoCanal,Banco.getCanales().indexOf(this) + 1,impuesto);
+		return "Canal: %s #%s\nTasa de impuestos: %s\n".formatted(tipoCanal, Banco.getCanales().indexOf(this) + 1, impuesto);
 	}
-	
-    public boolean tieneDivisa(Divisa divisa) {
-        return !fondosPorDivisa.containsKey(divisa);
-    }
-    
-    public boolean tieneFondosDeDivisa(Divisa divisa) {
+
+	/**
+	 * Verifica si el canal tiene una divisa específica.
+	 *
+	 * @param divisa La divisa a verificar.
+	 * @return true si el canal tiene la divisa, false en caso contrario.
+	 */
+	public boolean tieneDivisa(Divisa divisa) {
+		return !fondosPorDivisa.containsKey(divisa);
+	}
+
+	/**
+	 * Verifica si el canal tiene fondos disponibles para una divisa específica.
+	 *
+	 * @param divisa La divisa a verificar.
+	 * @return true si el canal tiene fondos disponibles para la divisa, false en caso contrario.
+	 */
+	public boolean tieneFondosDeDivisa(Divisa divisa) {
 		return !(fondosPorDivisa.get(divisa) > 0.0);
 	}
-    
-    public Transaccion finalizarConversion(Transaccion transaccion, double montoInicial){
-		if(transaccion.isRechazado()) 
+
+	/**
+	 * Finaliza la conversión de una transacción.
+	 * Realiza los cambios necesarios en las tarjetas y fondos del canal.
+	 *
+	 * @param transaccion     La transacción a finalizar.
+	 * @param montoInicial    El monto inicial de la transacción.
+	 * @return La transacción finalizada.
+	 */
+	public Transaccion finalizarConversion(Transaccion transaccion, double montoInicial) {
+		if (transaccion.isRechazado())
 			transaccion.getTarjetaOrigen().anadirTransaccionRechazada();
-		
-		if(!transaccion.isPendiente()) 
+
+		if (!transaccion.isPendiente())
 			return null;
-		
+
 		Divisa divisaOrigen = transaccion.getDivisa();
 		double fondosOrigen = transaccion.getCanal().getFondos(divisaOrigen);
-		
+
 		Divisa divisaDestino = transaccion.getTarjetaObjetivo().getDivisa();
 		double fondosDestino = transaccion.getCanal().getFondos(divisaDestino);
-		
-		transaccion.getTarjetaOrigen().sacarDinero(montoInicial);//Sacando dinero de la tarjeta de origen
-		transaccion.getCanal().setFondos(divisaOrigen, fondosOrigen + montoInicial);//Ingresando el dinero de la divisa de origen al canal
-		transaccion.getCanal().setFondos(divisaDestino, fondosDestino - transaccion.getCantidad());//Sacando dinero de la divisa de destino del canal
-		transaccion.getTarjetaObjetivo().introducirDinero(transaccion.getCantidad());//Ingresando la divisa de destino a la tarjeta de destino
+
+		transaccion.getTarjetaOrigen().sacarDinero(montoInicial); // Sacando dinero de la tarjeta de origen
+		transaccion.getCanal().setFondos(divisaOrigen, fondosOrigen + montoInicial); // Ingresando el dinero de la divisa de origen al canal
+		transaccion.getCanal().setFondos(divisaDestino, fondosDestino - transaccion.getCantidad()); // Sacando dinero de la divisa de destino del canal
+		transaccion.getTarjetaObjetivo().introducirDinero(transaccion.getCantidad()); // Ingresando la divisa de destino a la tarjeta de destino
 		transaccion.setPendiente(false);
-				
+
 		return transaccion;
 	}
 
-	public static ArrayList<Canal> seleccionarCanal(Divisa divisa, boolean retirar){ //Encuentra los canales apropiados para la transaccion, en el contexto de la funcionalidad "retirar o depositar dinero"
+	/**
+	 * Selecciona los canales apropiados para una transacción en función de la divisa y la operación (retirar o depositar dinero).
+	 *
+	 * @param divisa   La divisa de la transacción.
+	 * @param retirar  true si la operación es retirar dinero, false si es depositar dinero.
+	 * @return Una lista de los canales apropiados para la transacción.
+	 */
+	public static ArrayList<Canal> seleccionarCanal(Divisa divisa, boolean retirar) {
 		ArrayList<Canal> retorno = new ArrayList<>();
-		if(retirar){
-			for(Canal c : Banco.getCanales()){
-				if(!c.tieneDivisa(divisa)){
+		if (retirar) {
+			for (Canal c : Banco.getCanales()) {
+				if (!c.tieneDivisa(divisa)) {
 					retorno.add(c);
 				}
 			}
-		}else{
-			for(Canal c : Banco.getCanales()){
-				if(!c.tieneDivisa(divisa)){
-					if(!c.tieneFondosDeDivisa(divisa)){  //Estos chequeos deben hacerce uno después del otro, de otra manera, existe la posibilidad de que el programa lanze un error en caso de que el canal no tenga la divisa
+		} else {
+			for (Canal c : Banco.getCanales()) {
+				if (!c.tieneDivisa(divisa)) {
+					if (!c.tieneFondosDeDivisa(divisa)) { // Estos chequeos deben hacerse uno después del otro, de otra manera, existe la posibilidad de que el programa lance un error en caso de que el canal no tenga la divisa
 						retorno.add(c);
 					}
 				}
@@ -119,7 +179,16 @@ public class Canal implements Serializable{
 		return retorno;
 	}
 
-	public Transaccion generarTransaccion(Tarjeta tarjeta, double monto, Cliente cliente, boolean retirar){
+	/**
+	 * Genera una nueva transacción con los parámetros especificados.
+	 *
+	 * @param tarjeta  La tarjeta asociada a la transacción.
+	 * @param monto    El monto de la transacción.
+	 * @param cliente  El cliente que realiza la transacción.
+	 * @param retirar  true si la operación es retirar dinero, false si es depositar dinero.
+	 * @return La nueva transacción generada.
+	 */
+	public Transaccion generarTransaccion(Tarjeta tarjeta, double monto, Cliente cliente, boolean retirar) {
 		return new Transaccion(cliente, tarjeta, monto, this, retirar);
 	}
 }
